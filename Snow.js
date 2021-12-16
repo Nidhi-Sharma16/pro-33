57e43db00b835d97355cdb0af2b1773fa9caf754
Nidhi-Sharma16 <79085182+Nidhi-Sharma16@users.noreply.github.com>
2021-12-16 17:56:15 +0530

    Add files via upload

---
+++ Snow.js
@@ -0,0 +1,24 @@
+class Snow{
+    constructor(x, y, r) {
+        var options = {
+            'restitution':0.3,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.r=r;
+        this.image=loadImage("snow5.webp")
+        this.body = Bodies.circle(x, y,this.r, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.r*2, this.r*2);
+        pop();
+      }
+}
---
+++ sketch.js
@@ -0,0 +1,31 @@
+const Engine=Matter.Engine;
+const World=Matter.World;
+const Bodies=Matter.Bodies;
+
+var snow2Img;
+var engine,world;
+var snow=[];
+
+function preload(){
+  snow2Img = loadImage("snow2.jpg")
+}
+
+function setup() {
+  createCanvas(800,400);
+ engine=Engine.create();
+ world=engine.world;
+}
+
+function draw() {
+  Engine.update(engine)
+  background(snow2Img); 
+  if(frameCount%5===0){
+snow.push(new Snow(random(0,800),0,20));
+  } 
+  for(var i=0;i<snow.length;i++){
+    snow[i].display();
+  }
+  drawSprites();
+}
+
+ 
